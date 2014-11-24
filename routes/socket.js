fb14badc583ed681dfedd3382c8494a98aed4b40
Eric Allard <eric.allard0104@gmail.com>
2014-11-24 11:50:11 +0100

    bootstrap integration [design]

--- routes/socket.js
+++ routes/socket.js
@@ -6,10 +6,13 @@
 module.exports = function (io) {
   'use strict';
   var connectedUsers = {};
+  var sockets = {};
   io.on('connection', function (socket) {
     var id = null;
+
     socket.on('disconnect', function(){
       delete connectedUsers["id" + id]; 
+      delete sockets["id" + id]; 
       console.log("user disconnect id:" + id);
       io.sockets.emit('users:add', connectedUsers);
     });
@@ -19,12 +22,20 @@ module.exports = function (io) {
       socket.join(user.chatroom);
       id = user.id;
       connectedUsers["id" + id] = user;
+      sockets["id" + id] = socket;
       io.sockets.emit('users:add', connectedUsers);
     });
 
     socket.on('chatroom:change', function(data){
       socket.leave(data.old);
       socket.join(data.new);
+    });
+
+    socket.on('chatroom:invite', function(data){
+      for (k in data.usersId){
+        var id = data.usersId[k];
+        sockets["id"+id].emit('chatroom:invite', {chatroom: data.chatroom, user: data.user});
+      }
     });
 
     socket.on('message:new', function (msg) {
